93a59a5f1640eb55d9d6d435d52c923a497a2d06
Gerald-GG <gicharugerald@gmail.com>
2024-01-12 12:49:35 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import relationship
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -9,32 +10,25 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 class Bakery(db.Model, SerializerMixin):
-    __tablename__ = 'bakeries'
+    __tablename__ = "bakeries"
 
-    serialize_rules = ('-baked_goods.bakery',)
+    serialize_rules = ("-baked_goods.bakery",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    baked_goods = db.relationship('BakedGood', backref='bakery')
-
-    def __repr__(self):
-        return f'<Bakery {self.name}>'
-
 class BakedGood(db.Model, SerializerMixin):
-    __tablename__ = 'baked_goods'
+    __tablename__ = "baked_goods"
 
-    serialize_rules = ('-bakery.baked_goods',)
+    serialize_rules = ("-bakery.baked_goods",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey("bakeries.id"))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
-
-    def __repr__(self):
-        return f'<Baked Good {self.name}, ${self.price}>'
+    bakery = relationship("Bakery", backref="baked_goods")
